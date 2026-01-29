467cf692e3d84cc9ef4bc4ab08f0c0ffcc70f833
Jiří Hošek <jiri.hosek@argo22.com>
2026-01-29 15:21:44 +0100

    feat: improve and prepare modifySvg test for implementation rewrite

--- tests/modifySvg.test.ts
+++ tests/modifySvg.test.ts
@@ -5,22 +5,47 @@ type Options = Parameters<typeof modifySvg>[0];
 
 describe("modifySvg", () => {
 	it("should add vector-effect attribute to path element", async () => {
-		const spriteContent = "<svg><path></path></svg>";
+		const spriteContent1 = "<svg><path /></svg>";
+		const spriteContent2 = '<svg><path d="..."/></svg>';
+		const spriteContent3 =
+			'<svg><path d="..." vector-effect="non-scaling-stroke" fill="green"/></svg>';
+		const spriteContent4 =
+			'<svg><path d="..." vector-effect="none" fill="green"/></svg>';
 		const cssClassName = "test-icon";
-		const options: Options = {
-			spriteContent,
+
+		const result1 = await modifySvg({
+			spriteContent: spriteContent1,
 			cssClassName,
-		};
+		});
+		const result2 = await modifySvg({
+			spriteContent: spriteContent2,
+			cssClassName,
+		});
+		const result3 = await modifySvg({
+			spriteContent: spriteContent3,
+			cssClassName,
+		});
+		const result4 = await modifySvg({
+			spriteContent: spriteContent4,
+			cssClassName,
+		});
 
-		const result = await modifySvg(options);
-
-		expect(result).toBe(
-			'<svg><path vector-effect="non-scaling-stroke"></path></svg>',
+		expect(result1).toMatchInlineSnapshot(
+			`"<svg><path vector-effect="non-scaling-stroke" /></svg>"`,
+		);
+		expect(result2).toMatchInlineSnapshot(
+			`"<svg><path vector-effect="non-scaling-stroke" d="..."/></svg>"`,
+		);
+		expect(result3).toMatchInlineSnapshot(
+			`"<svg><path d="..." vector-effect="non-scaling-stroke" fill="green"/></svg>"`,
+		);
+		expect(result4).toMatchInlineSnapshot(
+			`"<svg><path d="..." vector-effect="non-scaling-stroke" fill="green"/></svg>"`,
 		);
 	});
 
 	it('should replace fill="none" by CSS variable', async () => {
-		const spriteContent = '<svg><symbol fill="none"></symbol></svg>';
+		const spriteContent = '<svg><symbol fill="none"/></svg>';
 		const cssClassName = "test-icon";
 		const options: Options = {
 			spriteContent,
@@ -30,12 +55,12 @@ describe("modifySvg", () => {
 		const result = await modifySvg(options);
 
 		expect(result).toBe(
-			`<svg><symbol fill="var(--${cssClassName}-fill-svg)"></symbol></svg>`,
+			`<svg><symbol fill="var(--${cssClassName}-fill-svg)"/></svg>`,
 		);
 	});
 
 	it("should replace fill color by CSS variable", async () => {
-		const spriteContent = '<svg><symbol fill="currentColor"></symbol></svg>';
+		const spriteContent = '<svg><symbol fill="currentColor"/></svg>';
 		const cssClassName = "test-icon";
 		const options: Options = {
 			spriteContent,
@@ -45,12 +70,12 @@ describe("modifySvg", () => {
 		const result = await modifySvg(options);
 
 		expect(result).toBe(
-			`<svg><symbol fill="var(--${cssClassName}-fill)"></symbol></svg>`,
+			`<svg><symbol fill="var(--${cssClassName}-fill)"/></svg>`,
 		);
 	});
 
 	it("should replace stroke color by CSS variable", async () => {
-		const spriteContent = '<svg><symbol stroke="currentColor"></symbol></svg>';
+		const spriteContent = '<svg><symbol stroke="currentColor"/></svg>';
 		const cssClassName = "test-icon";
 		const options: Options = {
 			spriteContent,
@@ -60,12 +85,43 @@ describe("modifySvg", () => {
 		const result = await modifySvg(options);
 
 		expect(result).toBe(
-			`<svg><symbol stroke="var(--${cssClassName}-stroke)"></symbol></svg>`,
+			`<svg><symbol stroke="var(--${cssClassName}-stroke)"/></svg>`,
 		);
 	});
 
 	it("should replace stroke-width by CSS variable", async () => {
-		const spriteContent = '<svg><symbol stroke-width="12"></symbol></svg>';
+		const spriteContent1 = '<svg><symbol stroke-width="12"/></svg>';
+		const spriteContent2 = '<svg><symbol stroke-width="12px"/></svg>';
+		const spriteContent3 = '<svg><symbol stroke-width="12%"/></svg>';
+		const cssClassName = "test-icon";
+
+		const result1 = await modifySvg({
+			spriteContent: spriteContent1,
+			cssClassName,
+		});
+		const result2 = await modifySvg({
+			spriteContent: spriteContent2,
+			cssClassName,
+		});
+		const result3 = await modifySvg({
+			spriteContent: spriteContent3,
+			cssClassName,
+		});
+
+		const expectedResult = `<svg><symbol stroke-width="var(--${cssClassName}-stroke-width)"/></svg>`;
+		expect(result1).toBe(expectedResult);
+		expect(result2).toBe(expectedResult);
+		expect(result3).toBe(expectedResult);
+	});
+
+	it("should not modify any text content", async () => {
+		const spriteContent = `<svg><symbol><desc>
+			vector-effect="none"
+			fill="none"
+			fill="currentColor"
+			stroke="currentColor"
+			stroke-width="12"
+		</desc></symbol></svg>`;
 		const cssClassName = "test-icon";
 		const options: Options = {
 			spriteContent,
@@ -74,8 +130,6 @@ describe("modifySvg", () => {
 
 		const result = await modifySvg(options);
 
-		expect(result).toBe(
-			`<svg><symbol stroke-width="var(--${cssClassName}-stroke-width)"></symbol></svg>`,
-		);
+		expect(result).toBe(spriteContent);
 	});
 });
